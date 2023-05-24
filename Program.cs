3e1a1f2961984976abd3f7c4f5e08340f95b0bbf
LadurAnna <131361803+LadurAnna@users.noreply.github.com>
2023-05-24 11:33:02 +0300

    DZ2405

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// _____Домашняя работа Задача 2_____________
+// Напишите программу, которая на вход принимает
+//  два числа и выдаёт, какое число большее, 
+//  а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число");
+//  int a=Convert.ToInt32(Console.ReadLine());
+
+
+// Console.WriteLine("Введите второе число");
+// int b=Convert.ToInt32(Console.ReadLine());
+
+// if (a>b)
+// {
+//    Console.WriteLine($"max={a}");
+// }
+
+// else if (b>a)
+// {
+//     Console.WriteLine($"max={b}");
+// }
+// else
+// {
+//     Console.WriteLine("Числа равны");
+// }
+
+// __________________Домашняя работа 4________________
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите число");
+//  int a=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число");
+// int b=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третье число");
+// int c=Convert.ToInt32(Console.ReadLine());
+
+// if (a>b)
+// {
+//    if (a>c) 
+//    {
+//       Console.WriteLine($"max={a}");
+//    }
+//    else
+//    {
+//     Console.WriteLine($"max={c}");
+//    }
+
+// }
+// else
+// {
+//     if (b>c)
+//     {
+//         Console.WriteLine($"max={b}");
+//     }
+//    else
+//    {
+//     Console.WriteLine($"max={c}");
+//    }
+// }
+// __________________Домашняя работа 6________________
+// Задача 6: Напишите программу, которая на вход принимает 
+// число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 1)
+// {
+//  Console.WriteLine("Число нечетное");
+// }
+//  else 
+// {
+//     Console.WriteLine("Число четное");
+// }
+
+// __________________Домашняя работа 6________________
+// Задача 8: Напишите программу, которая на 
+// вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= num; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//       Console.WriteLine(i);
+//     }  
+// }
+
